0533840c8f44c496147b3cf5b7b7ca4d51390a00
souljer4-github <88750088+souljer4-github@users.noreply.github.com>
2022-02-19 14:35:00 -0500

    Update While.py

--- While.py
+++ While.py
@@ -1,4 +1,4 @@
-class Car:
+class Vehicle:
     def __init__(self, year, make, model, fuel, doors):
         self.year = year
         self.make = make
@@ -6,10 +6,21 @@ class Car:
         self.fuel = fuel
         self.doors = doors
 
-    def vehicle(self):
-        description =    f'Year:          :{self.year}\n'\
-                         f'Make:          :{self.make}\n'\
-                         f'Model:         :{self.model}\n'\
-                         f'Fuel:          :{self.fuel}\n'\
-                         f'Doors:         :{self.doors}\n'\
+    def info(self):
+        description = f'Year: {self.year}\n' \
+                      f'Make: {self.make}\n' \
+                      f'Model: {self.model}\n' \
+                      f'Fuel: {self.fuel}\n' \
+                      f'Doors: {self.doors}\n'
         return description
+
+
+car1 = Vehicle('1998', 'Nissan', 'Sentra', 'Unleaded 87', 'Four doors')
+car2 = Vehicle('2001', 'Ford', 'Escort', 'Unleaded 87', 'Two Doors')
+car3 = Vehicle('2013', 'Toyota', 'Prius V', 'Unleaded 87', 'Five Doors')
+car4 = Vehicle('2013', 'Hyundai', 'Elantra', 'Unleaded 87', 'Two Doors')
+
+print(car1.info())
+print(car2.info())
+print(car3.info())
+print(car4.info())
