91794334f8ee8a2ad10499a43658bba3c2d22be0
souljer4-github <88750088+souljer4-github@users.noreply.github.com>
2022-02-19 14:08:46 -0500

    Update While.py

--- While.py
+++ While.py
@@ -1,21 +1,15 @@
-game = input("Would you like to play a game? ")
-game = game.lower()
-if game == "no":
-    print("Thanks for playing!")
-else:
-    while game == "yes" or "y":
-        color = input("What is your favorite color? ")
-        age = int(input("How old are you? "))
-        name = input("What is your name? ")
-        pd1 = {"Name": [], "age": [], "Color": []}
-        pd1["Name"].append(name)
-        pd1["age"].append(age)
-        pd1["Color"].append(color)
-        print(pd1)
-        check = str(input("Do you want to start over?"))
-        check = check.lower()
-        if check == "yes" or "y":
-            continue
-        if check == "no" or "n":
-            break
-        print("Thanks for playing!")
+class Car:
+    def __init__(self, year, make, model, fuel, doors):
+        self.year = year
+        self.make = make
+        self.model = model
+        self.fuel = fuel
+        self.doors = doors
+
+    def vehicle(self):
+        description =    f'Year:          :{self.year}\n'\
+                         f'Make:          :{self.make}\n'\
+                         f'Model:         :{self.model}\n'\
+                         f'Fuel:          :{self.fuel}\n'\
+                         f'Doors:         :{self.doors}\n'\
+        return description
