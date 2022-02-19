1dae50f052070090233240c3f7b1fb854af07d04
souljer4-github <88750088+souljer4-github@users.noreply.github.com>
2022-02-19 14:58:09 -0500

    Update While.py

--- While.py
+++ While.py
@@ -1,26 +1,38 @@
 class Vehicle:
-    def __init__(self, year, make, model, fuel, doors):
+    def __init__(self, year, make, model, fuel, doors, trans):
         self.year = year
         self.make = make
         self.model = model
         self.fuel = fuel
         self.doors = doors
-
-    def info(self):
+        self.trans = trans
+    def ymm(self):
         description = f'Year: {self.year}\n' \
                       f'Make: {self.make}\n' \
-                      f'Model: {self.model}\n' \
-                      f'Fuel: {self.fuel}\n' \
-                      f'Doors: {self.doors}\n'
+                      f'Model: {self.model}\n'
         return description
-
-
-car1 = Vehicle('1998', 'Nissan', 'Sentra', 'Unleaded 87', 'Four doors')
-car2 = Vehicle('2001', 'Ford', 'Escort', 'Unleaded 87', 'Two Doors')
-car3 = Vehicle('2013', 'Toyota', 'Prius V', 'Unleaded 87', 'Five Doors')
-car4 = Vehicle('2013', 'Hyundai', 'Elantra', 'Unleaded 87', 'Two Doors')
-
-print(car1.info())
-print(car2.info())
-print(car3.info())
-print(car4.info())
+    def fdt(self):
+        info = f'Fuel: {self.fuel}\n' \
+               f'Doors: {self.doors}\n' \
+               f'Transmission: {self.trans}'
+        return info
+car1 = Vehicle('1998', 'Nissan', 'Sentra', 'Unleaded 87', 'Four doors', 'Automatic')
+car2 = Vehicle('2001', 'Ford', 'Escort', 'Unleaded 87', 'Two Doors', 'Manual')
+car3 = Vehicle('2013', 'Toyota', 'Prius V', 'Unleaded 87', 'Five Doors', 'Automatic')
+car4 = Vehicle('2013', 'Hyundai', 'Elantra', 'Unleaded 87', 'Two Doors', 'Manual')
+print(car1.ymm(), end=' ')
+print(car1.fdt(), end=' ')
+print()
+print()
+print(car2.ymm(), end=' ')
+print(car2.fdt(), end=' ')
+print()
+print()
+print(car3.ymm(), end=' ')
+print(car3.fdt(), end=' ')
+print()
+print()
+print(car4.ymm(), end=' ')
+print(car4.fdt(), end=' ')
+print()
+print()
